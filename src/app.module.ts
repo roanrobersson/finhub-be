011d343b71c7bd36addcb56db62020f2d82d755a
Roan de Oliveira <roanrobersson@gmail.com>
2024-12-20 11:25:04 -0300

    feat: ✨ configure authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-	imports: [],
+	imports: [
+		AuthModule,
+		UsersModule,
+		ThrottlerModule.forRoot([
+			{
+				ttl: 60000,
+				limit: 10
+			}
+		])
+	],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard
+		}
+	]
 })
 export class AppModule {}
